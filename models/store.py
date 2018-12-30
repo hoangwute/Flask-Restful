385a61b94fc84005f2a033549c2b11e1399485c2
hoangwute <quochoang.kt26@gmail.com>
2018-12-30 17:29:49 +0700

    Add find_all method to models

--- models/store.py
+++ models/store.py
@@ -18,6 +18,10 @@ class StoreModel(db.Model):
 	def find_by_name(cls, name):
 		return cls.query.filter_by(name=name).first() #SELECT * FROM items WHERE name=name LIMIT 1
 
+	@classmethod
+	def find_all(cls):
+		return cls.query.all()
+
 	def save_to_db(self): #both inserting and updating
 		db.session.add(self)
 		db.session.commit()
