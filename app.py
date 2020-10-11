4a93551e52b962c4bacf46de1e47da616eb932c3
hoangwute <quochoang.kt26@gmail.com>
2020-10-12 00:47:35 +0700

    Use sqlite

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
@@ -25,6 +25,5 @@ api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':  # prevent app from running if other classes import something from app.py
     from db import db
-
     db.init_app(app)
     app.run(port=5000, debug=True)
